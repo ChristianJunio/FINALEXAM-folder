a2f3ceb0f75c55629ef7c0258555b31b179f1659
Christian Junio <98379073+ChristianJunio@users.noreply.github.com>
2022-04-28 20:41:12 +0800

    Add files via upload

---
+++ 2b.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ <title>Using document.write</title>
+ </head>
+ <body>
+ <h2>Christian Junio</h2>
+ <h2>BS Computer Science</h2>
+ <h2>1st Year</h2>
+ <h2>HUMCOM - IAA1</h2>
+ <script>
+ document.write("Introduction to Javascript");
+ </script>
+ </body>
+</html>
